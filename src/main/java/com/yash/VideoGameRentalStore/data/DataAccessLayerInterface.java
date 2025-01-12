9c134e836d57a60d8a4bd8ef7886f474b0b06660
yash <yash.sangani.yt@gmail.com>
2025-01-12 21:17:11 +0000

    accidentally forgot to create the most important part (getAllfrom file) + added comments to everything

--- src/main/java/com/yash/VideoGameRentalStore/data/DataAccessLayerInterface.java
+++ src/main/java/com/yash/VideoGameRentalStore/data/DataAccessLayerInterface.java
@@ -3,13 +3,16 @@ package com.yash.VideoGameRentalStore.data;
 import com.yash.VideoGameRentalStore.model.entity.Customer;
 import com.yash.VideoGameRentalStore.model.entity.Rental;
 import com.yash.VideoGameRentalStore.model.entity.VideoGame;
+import com.yash.VideoGameRentalStore.service.FileUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public interface DataAccessLayerInterface {
 
-    List<Customer> getAllCustomers();
+    List<Customer> getAllCustomers(); // this is a method signature to get all customers
 
     List<VideoGame> getAllVideoGames();
 
@@ -29,5 +32,23 @@ public interface DataAccessLayerInterface {
 
     int create_next_id(String filename) throws IOException;
 
+    // Generic method to read data from a file and convert each line to a target entity
+    private <T> List<T> getAllFromFile(String filename, Function<String, T> fromStringFunction) {
+        // Create a new list to hold the target entities
+        final List<T> targetEntities = new ArrayList<>();
 
+        // Load the file lines using the FileUtils.loadData method
+        List<String> fileLines = FileUtils.loadData(filename);
+
+        // Iterate over each line in the file
+        fileLines.forEach(fileLine -> {
+            // Convert the file line to a target entity using the provided function
+            T TargetEntity = fromStringFunction.apply(fileLine);
+            // Add the target entity to the list
+            targetEntities.add(TargetEntity);
+        });
+
+        // Return the list of target entities
+        return targetEntities;
+    }
 }
