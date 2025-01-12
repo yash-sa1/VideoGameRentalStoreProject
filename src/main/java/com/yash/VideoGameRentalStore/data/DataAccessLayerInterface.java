bdadbc461e809178e3297d05bfd8a82a6240fb1c
yash <yash.sangani.yt@gmail.com>
2025-01-12 21:05:18 +0000

    all the objectives that would be called from the dataaccesslayer

--- src/main/java/com/yash/VideoGameRentalStore/data/DataAccessLayerInterface.java
+++ src/main/java/com/yash/VideoGameRentalStore/data/DataAccessLayerInterface.java
@@ -1,4 +1,33 @@
 package com.yash.VideoGameRentalStore.data;
 
+import com.yash.VideoGameRentalStore.model.entity.Customer;
+import com.yash.VideoGameRentalStore.model.entity.Rental;
+import com.yash.VideoGameRentalStore.model.entity.VideoGame;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface DataAccessLayerInterface {
+
+    List<Customer> getAllCustomers();
+
+    List<VideoGame> getAllVideoGames();
+
+    List<Rental> getAllRentals(List<VideoGame> videoGames, List<Customer> customers);
+
+    void saveRental(Rental rental);
+
+    void changeStockAfterRenting (VideoGame game) throws IOException;
+
+    void changeStockAfterReturning (VideoGame game) throws IOException;
+
+    boolean checkRentalID(Rental rental , String entryToCheck) throws IOException;
+
+    List<String> readAndPrintRentalsFile(String customerID) throws IOException;
+
+    void registerCustomer(Customer customer) throws IOException;
+
+    int create_next_id(String filename) throws IOException;
+
+
 }
